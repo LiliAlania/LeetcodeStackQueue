ad05c411f469a16593ac2483e7142b6324d3e666
LiliAlania <alaniia.pn@ucu.edu.ua>
2025-04-14 14:20:56 +0300

    implementation of apppend and popleft methods in Deque class

--- Implement Stack using Queues /solution.py
+++ Implement Stack using Queues /solution.py
@@ -8,3 +8,23 @@ class Deque:
         self.front = None
         self.rear = None
         self._size = 0
+    def append(self, value):
+        new_node = Node(value)
+        if self.is_empty():
+            self.front = self.rear = new_node
+        else:
+            new_node.prev = self.rear
+            self.rear.next = new_node
+            self.rear = new_node
+        self._size += 1
+    def popleft(self):
+        if self.is_empty():
+            raise IndexError("pop_front from empty deque")
+        value = self.front.value
+        self.front = self.front.next
+        if self.front:
+            self.front.prev = None
+        else:
+            self.rear = None
+        self._size -= 1
+        return value
