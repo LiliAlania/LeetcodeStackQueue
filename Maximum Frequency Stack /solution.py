3f83d41874c495c274028d733a192f7371203e76
LiliAlania <alaniia.pn@ucu.edu.ua>
2025-04-14 15:16:19 +0300

    updated pop method and init and push with defaultdict

--- Maximum Frequency Stack /solution.py
+++ Maximum Frequency Stack /solution.py
@@ -1,33 +1,25 @@
-from collections import deque
+from collections import deque, defaultdict
 class FreqStack(object):
     def __init__(self):
         self.elements = deque()
-
+        self.freq = defaultdict(int)
     def push(self, val):
         """
         :type val: int
         :rtype: None
         """
         self.elements.append(val)
+        self.freq[val] += 1
     def pop(self):
         """
         :rtype: int
         """
-        el_dict = {}
-        len_ = 0
-        max_el = None
-        for element in self.elements:
-            len_ += 1
-            if element not in el_dict.keys():
-                el_dict[element] = [0, len_]
-            else:
-                el_dict[element] += [1, len_]
-        max_fr = max(el_dict.values()[0])
-        for el in el_dict.keys():
-            if el_dict[el][0] == max_fr:
-                if max_el:
-                    if el_dict[el][1] < el_dict[max_el][0]:
-                        max_el = el
-                else:
-                    max_el = el
-        return self.element.pop(max_el)
+        max_freq = max(self.freq.values())
+        for i in range(len(self.elements) - 1, -1, -1):
+            val = self.elements[i]
+            if self.freq[val] == max_freq:
+                del self.elements[i]
+                self.freq[val] -= 1
+                if self.freq[val] == 0:
+                    del self.freq[val]
+                return val
