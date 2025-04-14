11925fc850413f5b628acddc3142b4546c69c6dd
LiliAlania <alaniia.pn@ucu.edu.ua>
2025-04-14 14:19:55 +0300

    deque class initialization

--- Implement Stack using Queues /solution.py
+++ Implement Stack using Queues /solution.py
@@ -3,3 +3,8 @@ class Node:
         self.value = value
         self.prev = None
         self.next = None
+class Deque:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self._size = 0
